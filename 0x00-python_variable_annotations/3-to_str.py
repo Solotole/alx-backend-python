78842898cb6c8d72aaa25cccdcb2e30abc8a957f
Solotole <solomontoleak47@gmail.com>
2024-08-15 10:22:28 +0100

    converting float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+
+
+def to_str(n: float) -> str:
+    """ changing float to a string and returning it """
+    string = str(n)
+    return string

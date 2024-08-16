7d9eecc428bd9f760d8b619c8339033b1e702e6a
Solotole <solomontoleak47@gmail.com>
2024-08-16 03:05:44 +0100

    Type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-""" Type Checking """
-from typing import List, Sequence, Tuple, Union
+"""Using mypy to validate code"""
+from typing import Tuple, List
 
 
-def zoom_array(
-    lst: Union[List[int], Tuple[int, ...]], factor: int = 2
-) -> List[int]:
-    """ Zoom array- returning tuple of an array """
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Function that takes list and factor and returns the zoom_in"""
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: Tuple = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 

185b1709e0b0c9e6cd7418a64e9782a491515325
Solotole <solomontoleak47@gmail.com>
2024-08-19 11:56:54 +0100

    creating a task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,7 +5,7 @@ from typing import Any, Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[Any]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ returning a creted task """
-    concurrent_obj: Awaitable[Any] = asyncio.create_task(wait_random(max_delay))
+    concurrent_obj: asyncio.Task = asyncio.create_task(wait_random(max_delay))
     return concurrent_obj

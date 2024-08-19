fef2d5d113040819c12239256423b03611384566
Solotole <solomontoleak47@gmail.com>
2024-08-19 11:46:38 +0100

    running an already created task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,14 +3,16 @@
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
-insertion_sort = __import__('1-concurrent_coroutines').insertion_sort
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ calling an already created task """
-    return_list = []
-    for _ in range(n):
-        value = await task_wait_random(max_delay)
-        return_list.append(value)
-    return_list = insertion_sort(return_list)
-    return return_list
+    delays: List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        i = 0
+        while i < len(delays) and delays[i] < delay:
+            i += 1
+        delays.insert(i, delay)
+    return delays

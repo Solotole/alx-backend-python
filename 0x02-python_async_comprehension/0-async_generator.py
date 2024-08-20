9ff117b86873a8fd3aa1c4f266209d777e28b0ed
Solotole <solomontoleak47@gmail.com>
2024-08-20 12:47:45 +0100

    yielding in Async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Basic async generator """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, float]:
+async def async_generator() -> Generator[float, None, None]:
     """ randomly generating a float in a generator """
     for _ in range(10):
         await asyncio.sleep(1)

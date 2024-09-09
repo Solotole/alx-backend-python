f27fcbf525df2c64b0d7c0e5dbb7ff3f016b7cb4
Solotole <solomontoleak47@gmail.com>
2024-09-09 05:16:19 +0100

    testing parameters

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 """Parameterize a unit test"""
 import unittest
-from utils import *
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """class to carry out unittest"""
-    @parameterized.expand
-    def test_access_nested_map(self):
-        """Testing method Access nested map with key path"""
-        self.assertEqual(access_nested_map({"a": 1}, ["a"]), 1)
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a")), {"b": 2})
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a", "b")), 2)
+    """Class to carry out unittest for access_nested_map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Testing method access_nested_map with key path"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+if __name__ == "__main__":
+    unittest.main()

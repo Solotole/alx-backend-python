c6ec33cdaa80b321cddce8d7b406693d33bd8fdb
Solotole <solomontoleak47@gmail.com>
2024-09-09 04:58:21 +0100

    testing parameters

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,10 +5,10 @@ from utils import *
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    "class to carry out unittest"
+    """class to carry out unittest"""
     @parameterized.expand
     def test_access_nested_map(self):
-        "Testing method Access nested map with key path"
+        """Testing method Access nested map with key path"""
         self.assertEqual(access_nested_map({"a": 1}, ["a"]), 1)
         self.assertEqual(access_nested_map({"a": {"b": 2}}, ["a"]), {"b": 2})
         self.assertEqual(access_nested_map({"a": {"b": 2}}, ["a", "b"]), 2)
